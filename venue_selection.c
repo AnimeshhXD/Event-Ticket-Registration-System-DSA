010292d4ac95eedf51bfe412d953bc9bbcd5b67c
Animw <106533232+AnimeshhXD@users.noreply.github.com>
2025-10-30 16:17:32 +0530

    Update venue_selection.c

--- venue_selection.c
+++ venue_selection.c
@@ -1,21 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
-typedef struct VenueNode {
-    int venueID;
-    char venueName[50];
-    struct VenueNode* left;
-    struct VenueNode* right;
-} VenueNode;
+#include "venue_selection.h"
 
 VenueNode* insertVenue(VenueNode* root, int id, const char* name) {
     if (root == NULL) {
         VenueNode* newNode = (VenueNode*)malloc(sizeof(VenueNode));
-        if (!newNode) {
-            printf("Memory allocation failed\n");
-            return NULL;
-        }
         newNode->venueID = id;
         strcpy(newNode->venueName, name);
         newNode->left = newNode->right = NULL;
@@ -25,8 +15,6 @@ VenueNode* insertVenue(VenueNode* root, int id, const char* name) {
         root->left = insertVenue(root->left, id, name);
     else if (id > root->venueID)
         root->right = insertVenue(root->right, id, name);
-    else
-        printf("Venue ID %d already exists.\n", id);
     return root;
 }
 
