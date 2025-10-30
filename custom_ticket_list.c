f7745399cb564706393d6a8979a18c76f0608429
Animw <106533232+AnimeshhXD@users.noreply.github.com>
2025-10-30 16:16:45 +0530

    Update custom_ticket_list.c

--- custom_ticket_list.c
+++ custom_ticket_list.c
@@ -1,25 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
-typedef struct TicketNode {
-    int ticketID;
-    char ticketName[50];
-    struct TicketNode* next;
-} TicketNode;
+#include "custom_ticket_list.h"
 
 TicketNode* head = NULL;
 
 void addTicket(int id, const char* name) {
     TicketNode* newNode = (TicketNode*)malloc(sizeof(TicketNode));
-    if (!newNode) {
-        printf("Memory allocation failed\n");
-        return;
-    }
     newNode->ticketID = id;
     strcpy(newNode->ticketName, name);
     newNode->next = NULL;
-
     if (head == NULL) {
         head = newNode;
     } else {
@@ -33,7 +23,7 @@ void addTicket(int id, const char* name) {
 
 void displayTickets() {
     TicketNode* temp = head;
-    printf("Ticket List:\n");
+    printf("Tickets List:\n");
     while (temp != NULL) {
         printf("Ticket ID: %d, Name: %s\n", temp->ticketID, temp->ticketName);
         temp = temp->next;
